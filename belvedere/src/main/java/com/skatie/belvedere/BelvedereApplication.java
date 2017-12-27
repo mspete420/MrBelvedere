b5fcb94332bcec8b26a4338976e4a7111146c820
Scott Peters <scottpeters@gmail.com>
2017-12-27 13:44:13 -0600

    initial commit

---
+++ belvedere/src/main/java/com/skatie/belvedere/BelvedereApplication.java
@@ -0,0 +1,12 @@
+package com.skatie.belvedere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelvedereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelvedereApplication.class, args);
+	}
+}
